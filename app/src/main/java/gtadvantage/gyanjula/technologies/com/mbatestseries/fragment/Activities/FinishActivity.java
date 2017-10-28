f3adf1133cf5a4b00bcc36bad72822b940e7e179
sooraj2102 <srjshingari@gmail.com>
2017-10-28 23:50:41 +0530

    Update FinishActivity.java

--- app/src/main/java/gtadvantage/gyanjula/technologies/com/mbatestseries/fragment/Activities/FinishActivity.java
+++ app/src/main/java/gtadvantage/gyanjula/technologies/com/mbatestseries/fragment/Activities/FinishActivity.java
@@ -13,10 +13,12 @@ import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.MenuItem;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import android.widget.Toast;
 
 import com.github.lzyzsd.circleprogress.DonutProgress;
@@ -26,6 +28,7 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -42,11 +45,13 @@ import java.util.List;
 
 import gtadvantage.gyanjula.technologies.com.mbatestseries.R;
 
+
 public class FinishActivity extends AppCompatActivity {
 
     String PREF_NAME="level_pr";
     private DonutProgress donutProgress;
     ImageView sponsorImg;
+    
     TextView sponInfo,sponLink;
     TextView correct_ans,wrong_ans,acknow,level;
     Button correctans;
@@ -61,18 +66,22 @@ public class FinishActivity extends AppCompatActivity {
         text.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.white)), 0, text.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
 
         setTitleColor(getResources().getColor(R.color.white));
+        
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
         correct_ans=(TextView)findViewById(R.id.correct_ans);
         wrong_ans=(TextView)findViewById(R.id.wrong_ans);
+        
         level=(TextView)findViewById(R.id.level);
+        
         acknow=(TextView)findViewById(R.id.acknow);
         sponsorImg=(ImageView)findViewById(R.id.sponsorImg);
         sponInfo=(TextView)findViewById(R.id.sponsorInfo);
         sponLink=(TextView)findViewById(R.id.sponsorLink);
         donutProgress=(DonutProgress)findViewById(R.id.Progress);
+        
         correctans=(Button)findViewById(R.id.rightAns);
 
         if(getIntent().getExtras().getString("level_pr").equals("_")){
@@ -91,9 +100,11 @@ public class FinishActivity extends AppCompatActivity {
         });
 
         Intent intent=getIntent();
+        
         int score=intent.getIntExtra("score",0);
         int total_ques=intent.getIntExtra("total_ques",0);
         acknow.setText(intent.getStringExtra("acknow"));
+        
         donutProgress.setProgress((score*100)/total_ques);
         if(getIntent().getExtras().getString("level_pr").equals("_")){
             level.setVisibility(View.GONE);
@@ -118,13 +129,16 @@ public class FinishActivity extends AppCompatActivity {
         else{
             correct_ans.setText(String.valueOf(score));
         }
+        
 
         if(total_ques-score<10){
+            
             wrong_ans.setText("0"+String.valueOf(total_ques-score));
         }
         else{
             wrong_ans.setText(String.valueOf(total_ques-score));
         }
+        
 
        String paper=getIntent().getExtras().getString("paperId","");
         register_score(getIntent().getExtras().getString("mobile",""),String.valueOf(score),String.valueOf(total_ques),paper);
@@ -136,6 +150,7 @@ public class FinishActivity extends AppCompatActivity {
 
             private Dialog loadingDialog;
 
+            
             @Override
             protected void onPreExecute() {
                 super.onPreExecute();
@@ -145,15 +160,19 @@ public class FinishActivity extends AppCompatActivity {
             @Override
             protected String doInBackground(String... params) {
                 String mobile = params[0];
+                
                 String score=params[1];
                 String max_score=params[2];
+                
                 String paperId=params[3];
 
                 InputStream is = null;
                 List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-                nameValuePairs.add(new BasicNameValuePair("mobile_no", mobile));
-                nameValuePairs.add(new BasicNameValuePair("score", score));
+                
+                   nameValuePairs.add(new BasicNameValuePair("mobile_no", mobile));
+                   nameValuePairs.add(new BasicNameValuePair("score", score));
                 nameValuePairs.add(new BasicNameValuePair("max_score", max_score));
+                
                 nameValuePairs.add(new BasicNameValuePair("paperId", paperId));
 
                 String result = null;
@@ -161,6 +180,7 @@ public class FinishActivity extends AppCompatActivity {
                 try{
                     HttpClient httpClient = new DefaultHttpClient();
                     HttpPost httpPost = new HttpPost(
+                        
                             "http://gyanjulatechnologies.com/MbaTestSeries/index.php/TestSeries/score");
                     httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
